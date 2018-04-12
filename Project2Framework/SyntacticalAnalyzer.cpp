0e615c61ab0c5348adc006f253b75a223d71f578
Ian Matthew Davidson <idavidson@blue.cs.sonoma.edu>
2018-04-12 15:47:17 -0700

    initial skeletons made for Syn

--- Project2Framework/SyntacticalAnalyzer.cpp
+++ Project2Framework/SyntacticalAnalyzer.cpp
@@ -1,11 +1,16 @@
 /*******************************************************************************
 * Assignment: Project 2 - Syntactic Analyzer for Scheme to C++ Translator      *
-* Author:                                                                      *
+* Author:Ian Davidson & Andre Kuplian                                          *
 * Date: Spring 2018                                                            *
 * File: SyntacticalAnalyzer.cpp                                                *
 *                                                                              *
 * Description: This file contains the                                          *
 *******************************************************************************/
+
+
+
+//use external file to build all first and follows sets then include from here
+
 
 #include <iostream>
 #include <iomanip>
@@ -15,7 +20,7 @@
 using namespace std;
 
 /*******************************************************************************
-* Function:                                                                    *
+* Function: SyntacticialAnalyzer                                               *
 *                                                                              *
 * Parameters:                                                                  *
 * Return value:                                                                *
@@ -32,7 +37,7 @@ SyntacticalAnalyzer::SyntacticalAnalyzer (char * filename)
 }
 
 /*******************************************************************************
-* Function:                                                                    *
+* Function:  Deconstructor                                                     *
 *                                                                              *
 * Parameters:                                                                  *
 * Return value:                                                                *
@@ -45,7 +50,7 @@ SyntacticalAnalyzer::~SyntacticalAnalyzer ()
 }
 
 /*******************************************************************************
-* Function:                                                                    *
+* Function: Program                                                            *
 *                                                                              *
 * Parameters:                                                                  *
 * Return value:                                                                *
@@ -53,6 +58,8 @@ SyntacticalAnalyzer::~SyntacticalAnalyzer ()
 *******************************************************************************/
 int SyntacticalAnalyzer::Program ()
 {
+//definitly not done
+
 	p2file << "Entering Program function; current token is: "
 					<< lex->GetTokenName (token) << endl;
 	int errors = 0;
@@ -71,3 +78,316 @@ int SyntacticalAnalyzer::Program ()
 					<< lex->GetTokenName (token) << endl;
 	return errors;
 }
+
+
+
+/*******************************************************************************
+* Function: define                                                             *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::define ()
+{
+
+	cout << "Entering function: define() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+
+}
+
+
+
+/*******************************************************************************
+* Function: more_defines                                                       *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::more_defines ()
+{
+	
+	cout << "Entering function: more_defines() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+
+}
+
+
+
+
+/*******************************************************************************
+* Function: stmt_list                                                          *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::stmt_list ()
+{
+cout << "Entering function: stmt_list() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+int errors = 0;
+//check if current token is in first and follows of respective
+
+
+//do specific rule related procedure
+
+
+return errors;
+
+	
+}
+
+
+
+
+/*******************************************************************************
+* Function: stmt                                                               *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::stmt ()
+{
+
+	cout << "Entering function: stmt() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+
+}
+
+
+
+/*******************************************************************************
+* Function: literal                                                            *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::literal ()
+{
+	cout << "Entering function: literal() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+
+	
+}
+
+
+
+
+/*******************************************************************************
+* Function: quoted_lit                                                         *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::quoted_lit ()
+{
+	cout << "Entering function: quoted_lit() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+}
+
+/*******************************************************************************
+* Function: more_tokens                                                        *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::more_tokens ()
+{
+	cout << "Entering function: more_tokens() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+}
+
+
+
+/*******************************************************************************
+* Function: param_list                                                             *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::param_list ()
+{
+	cout << "Entering function: param_list() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+}
+
+
+
+
+
+
+/*******************************************************************************
+* Function: else_part                                                          *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::else_part ()
+{
+
+	cout << "Entering function: else_part() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+
+}
+
+
+
+/*******************************************************************************
+* Function: stmt_pair                                                          *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::stmt_pair()
+{
+	
+	cout << "Entering function: stmt_pair() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+
+}
+
+
+
+/*******************************************************************************
+* Function: stmt_pair_body                                                     *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::stmt_pair_body()
+{
+	cout << "Entering function: stmt_pair_body() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+
+	return errors;
+}
+
+
+
+/*******************************************************************************
+* Function: action                                                             *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::action()
+{
+
+	cout << "Entering function: action() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+	int errors = 0;
+	//check if current token is in first and follows of respective
+
+
+	//do specific rule related procedure
+
+
+	return errors;
+
+}
+
+
+/*******************************************************************************
+* Function: any_other_token                                                          *
+*                                                                              *
+* Parameters:                                                                  *
+* Return value:                                                                *
+* Description: This function will                                              *
+*******************************************************************************/
+int SyntacticalAnalyzer::any_other_token()
+{
+	
+cout << "Entering function: any_other_token() with current token: " << lex->GetTokenName(token) << endl; //enter funct name
+int errors = 0;
+//check if current token is in first and follows of respective
+
+
+//do specific rule related procedure
+
+
+return errors;
+
+}
+
+
+
+
+
